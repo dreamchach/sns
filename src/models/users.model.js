a9c122e91f1dd3e275b06bf52e99aa87aaf4a84b
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-09-09 14:29:43 +0900

    6. 비밀번호를 암호화하여 데이터베이스에 저장하는 방법

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     email : {
         type : String,
@@ -16,13 +17,29 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.methods.comparePassword = function (plainPassword, cb) {
-    if(plainPassword === this.password) {
-        cb(null, true)
-    } else {
-        cb(null, false)
+userSchema.pre('save', function(next) {
+    let user = this
+    
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(error, salt) {
+            if(error) return next(error)
+
+            bcrypt.hash(user.password, salt, function(error, hash) {
+                if(error) return next(error)
+
+                user.password = hash
+                next()
+            })
+        })
     }
-    return cb({error : 'error'})
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(error, isMatch) {
+        if(error) return cb(error)
+
+        cb(null, isMatch)
+    })
 }
 
 const User = mongoose.model('User', userSchema)
