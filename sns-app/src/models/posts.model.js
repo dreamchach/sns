37142e761cd3443fc3f13cfea898378edbe3b013
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-09-17 22:38:01 +0900

    post-item ui 생성

--- sns-app/src/models/posts.model.js
+++ sns-app/src/models/posts.model.js
@@ -1,7 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    description : String,
+    description : {
+        type : String,
+        required : true
+    },
     comments : [{
         type : mongoose.Schema.Types.ObjectId,
         ref : 'Comment'
