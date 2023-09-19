8aa4df557e7eb7eaa8e6fcf06869f98f53a5b4a6
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-09-19 17:46:07 +0900

    좋아요 기능 / 친구 페이지 ui
    - 좋아요 ui 구현
    - 좋아요 기능 구현
    - 친구 페이지 ui 구현

--- sns-app/src/routes/friends.router.js
+++ sns-app/src/routes/friends.router.js
@@ -1,4 +1,19 @@
 const express = require('express')
+const { checkAuthenticated } = require('../middlewares/auth')
 const router = express.Router()
+const User = require('../models/users.model')
+
+router.get('/', checkAuthenticated, async (req, res) => {
+    await User.find({})
+        .then((users) => {
+            res.render('friends/index', {
+                users : users
+            })
+        })
+        .catch((error) => {
+            req.flash('error', '유저를 가져오는데 에러가 발생했습니다')
+            res.redirect('/posts')
+        })
+})
 
 module.exports = router
