5598428a79cb5e4d60a919ff854728f554762765
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-09-20 21:32:02 +0900

    profile 페이지 구현
    - profile 페이지 UI 구현
    - profile 페이지 기능 구현
    - profile 수정 페이지 UI 구현
    - profile 수정 페이지 기능 구현

--- sns-app/src/middlewares/auth.js
+++ sns-app/src/middlewares/auth.js
@@ -1,5 +1,6 @@
 const Post = require('../models/posts.model')
 const Comment = require('../models/comments.model')
+const User = require('../models/users.model')
 
 // Protected Route
 function checkAuthenticated(req, res, next) {
@@ -67,9 +68,31 @@ function checkCommentOwnership(req, res, next) {
     }
 }
 
+function checkIsMe(req, res, next) {
+    if(req.isAuthenticated()) {
+        User.findById(req.params.id)
+            .then((user) => {
+                if(user._id.equals(req.user._id)) {
+                    next()
+                } else {
+                    req.flash('error', '권한이 없습니다')
+                    res.redirect('/profile/' + req.params.id)
+                }
+            })
+            .catch((error) => {
+                req.flash('error', '유저를 찾는데 에러가 발생했습니다')
+                res.redirect('/profile/' + req.params.id)
+            })
+    } else {
+        req.flash('error', '먼저 로그인을 해주세요')
+        res.redirect('/login')
+    }
+}
+
 module.exports = {
     checkAuthenticated,
     checkNotAuthenticated,
     checkPostOwnerShip,
-    checkCommentOwnership
+    checkCommentOwnership,
+    checkIsMe
 }
--- sns-app/src/routes/profiles.router.js
+++ sns-app/src/routes/profiles.router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router({mergeParams : true})
-const { checkAuthenticated } = require('../middlewares/auth')
+const { checkAuthenticated, checkIsMe } = require('../middlewares/auth')
 const Post = require('../models/posts.model')
 const User = require('../models/users.model')
 
@@ -24,6 +24,22 @@ router.get('/', checkAuthenticated, (req, res) => {
         })
         .catch((error) => {
             req.flash('error', '게시물을 가져오는데 실패했습니다')
+            res.redirect('back')
+        })
+})
+
+router.get('/edit', checkIsMe, (req, res) => {
+    res.render('profile/edit', {user : req.user})
+})
+
+router.put('/', checkIsMe, (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            req.flash('success', '유저 데이터를 업데이트하는데 성공했습니다')
+            res.redirect('/profile/' + req.params.id)
+        })
+        .catch((error) => {
+            req.flash('error', '유저 데이터를 업데이트하는데 에러가 발생했습니다')
             res.redirect('back')
         })
 })
