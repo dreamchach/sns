3c4a83b6fa4a7fc5bc13eef5771d75094681001a
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-09-20 13:52:52 +0900

    header UI / profile 페이지 구현
    - header UI 구현
    - profile 페이지 UI 구현
    - profile 정보 받아오는 api 구현

--- sns-app/src/routes/profiles.router.js
+++ sns-app/src/routes/profiles.router.js
@@ -1,4 +1,31 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router({mergeParams : true})
+const { checkAuthenticated } = require('../middlewares/auth')
+const Post = require('../models/posts.model')
+const User = require('../models/users.model')
+
+router.get('/', checkAuthenticated, (req, res) => {
+    Post.find({'author.id' : req.params.id})
+        .populate('comments')
+        .sort({createdAt : -1})
+        .exec()
+        .then((posts) => {
+            User.findById(req.params.id)
+                .then((user) => {
+                    res.render('profile/index', {
+                        posts : posts,
+                        user : user
+                    })
+                })
+                .catch((error) => {
+                    req.flash('error', '없는 유저입니다')
+                    res.redirect('back')
+                })
+        })
+        .catch((error) => {
+            req.flash('error', '게시물을 가져오는데 실패했습니다')
+            res.redirect('back')
+        })
+})
 
 module.exports = router
